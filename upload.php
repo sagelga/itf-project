f11d89e51944e9be3b739a2a0ff67705474030df
triggermagnum <dorayakiblackbean@gmail.com>
2016-10-22 20:35:32 +0700

    Fix Upload (Sync.Failed)
    I don't know Is it active or not, Lastest upload yesterday.

--- upload.php
+++ upload.php
@@ -78,6 +78,17 @@
     </nav>
     <h1 align="center">Upload files</h1>
   <hr>
+    <!-- This thing upload files to SQL -->
+  <center>
+        <font color="white">
+        <form name="form1" method="post" action="PageUploadToMySQL2.php" enctype="multipart/form-data">
+        Name : <input type="text" name="txtName"><br>
+        Picture : <input type="file" name="filUpload"><br>
+        <input name="btnSubmit" type="submit" value="Submit">
+        </font>
+        </form>
+  </center>
+
     <footer>
         <div class="container">
             <div class="row">
