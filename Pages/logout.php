b1bfee49b0721aa7a7d2dbc6e2999b05fdee8896
sagelga <son9912@gmail.com>
2016-10-31 19:57:10 +0700

    I don't know what's changed

--- Pages/logout.php
+++ Pages/logout.php
@@ -72,13 +72,14 @@
 </nav>
 
 <center><img src="web.sinchronize.com/images/loading.gif" width="50%" length="50%"></center>
+
 <?php
 session_start();
 unset($_SESSION["username"]);
 unset($_SESSION["password"]);
 
 echo <h2>'You have cleaned session'</h2>;
-header('Refresh: 2; URL = http://web.sinchronize.com/Pages/login.php');
+header('Refresh: 2; URL = http://web.sinchronize.com/');
 ?>
 
 <footer>
