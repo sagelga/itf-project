7ad1bced0e633c6639d03b38a48e4ef1fa1111a7
triggermagnum <dorayakiblackbean@gmail.com>
2016-11-03 15:01:38 +0700

    P'TA Template
    Template for connect to database.

---
+++ Pages/action_upload.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "upload";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO `picture`(`id`, `link`) VALUES ('".$_GET['id']."','".$_GET['title']."');";
+$result = $conn->query($sql);
+//echo '<img src="'.$row['link'].'">';
+
+//if ($result->num_rows > 0) {
+    // "output data of each row"
+    //while($row = $result->fetch_assoc()) {
+        //$sql2 = “SELECT  `comment` FROM `comment` WHERE `id_picture` =”.$row[‘id’];
+//$result2 = $conn->query($sql2);
+
+//    }
+//} else {
+//    echo "0 results";
+
+?>
---
+++ Pages/upload_test.php
@@ -0,0 +1,11 @@
+<form action="action_upload.php" method="get" ">
+
+
+           ID<input type="text" name="id"><br>
+           Title<input type="text" name="title"><br>
+           Link<input type="text" name="link"><br>
+           Description<input type="text" name="description"><br>
+
+        <button type="submit">Upload files</button>
+
+  </form>
