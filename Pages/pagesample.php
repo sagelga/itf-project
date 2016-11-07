f425b786d27821d6036e469563bfdbb3bcad8c95
son9912 <son9912@gmail.com>
2016-11-07 21:18:37 +0700

    This is my LAST COMMIT
    Good luck to y'all

---
+++ Pages/pagesample.php
@@ -0,0 +1,13 @@
+<?php $root = realpath($_SERVER["DOCUMENT_ROOT"]);
+include("$root/Templates/NavigationBar.html");?>
+
+<div class="col-xs-10 col-xs-offset-1">
+    <img align="center" src="http://ipod.item-get.com/img/16/11/NyanCat_TouchBar-02.jpg" alt="That is the reasons to buy the new MacBook Pro">
+    <h3 align="left">This is the title</h3>
+    <h5 align="left">This is the subtitle</h5>
+    <p align="right">Uploaded by</p><?php ?>
+    <button align="right">Click here to download</button>
+</div>
+
+<?php $root = realpath($_SERVER["DOCUMENT_ROOT"]);
+include("$root/Templates/FooterBar.html");?>
