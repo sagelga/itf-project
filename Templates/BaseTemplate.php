4f930104eb42d7a4ad721532f8537ff85a48035f
Sagelga <son9912@gmail.com>
2016-11-09 16:44:04 +0700

    Add some basic looks to the page
    What the fuck is localhost doing??

---
+++ Templates/BaseTemplate.php
@@ -0,0 +1,7 @@
+<?php 
+$root = realpath($_SERVER["DOCUMENT_ROOT"]); 
+include("$root/Templates/NavigationBar.html");?>
+
+<?php 
+$root = realpath($_SERVER["DOCUMENT_ROOT"]); 
+include("$root/Templates/FooterBar.html");?>
