9eaeecf5e5bf0d7f08f8da0a47c9eeab8f95a291
son9912 <son9912@gmail.com>
2016-11-09 19:18:01 +0700

    Broken Footer Has been fixed.

--- Templates/BaseTemplate.php
+++ Templates/BaseTemplate.php
@@ -1,7 +1,7 @@
-<?php 
-$root = realpath($_SERVER["DOCUMENT_ROOT"]); 
+<?php
+$root = realpath($_SERVER["DOCUMENT_ROOT"]);
 include("$root/Templates/NavigationBar.html");?>
-
-<?php 
-$root = realpath($_SERVER["DOCUMENT_ROOT"]); 
+<h1 align="center">This thing will be your content</h1>
+<?php
+$root = realpath($_SERVER["DOCUMENT_ROOT"]);
 include("$root/Templates/FooterBar.html");?>
